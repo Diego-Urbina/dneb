4725b6fd2916e35956c868763c4e1487bb437e7d
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-09-09 19:04:01 +0000

    Consigue bajarse una imagen!!!

---
+++ HttpImageDownloader/src/es/ucm/si/dneb/DownloadIt.java
@@ -0,0 +1,68 @@
+package es.ucm.si.dneb;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+public class DownloadIt {
+	
+	public static void downloadImage(String survey,String ascensionRecta,String declinacion,String equinocio,String alto,String ancho,String formato, String compresion) throws ClientProtocolException, IOException{
+		
+		
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		List<BasicNameValuePair> formparams = new ArrayList<BasicNameValuePair>();
+		formparams.add(new BasicNameValuePair("v", survey));
+		formparams.add(new BasicNameValuePair("r", ascensionRecta));
+		formparams.add(new BasicNameValuePair("d", declinacion));
+		formparams.add(new BasicNameValuePair("e", equinocio));
+		formparams.add(new BasicNameValuePair("h", alto));
+		formparams.add(new BasicNameValuePair("w", ancho));
+		formparams.add(new BasicNameValuePair("f", formato));
+		formparams.add(new BasicNameValuePair("c", compresion));
+		formparams.add(new BasicNameValuePair("fov", "none"));
+		formparams.add(new BasicNameValuePair("v3", ""));
+
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams,
+				"UTF-8");
+		HttpPost httppost = new HttpPost(
+				"http://archive.stsci.edu/cgi-bin/dss_search");
+		httppost.setEntity(entity);
+
+		HttpResponse response = httpclient.execute(httppost);
+		HttpEntity resEnt = response.getEntity();
+		if (resEnt != null) {
+			BufferedInputStream bis = new BufferedInputStream(resEnt.getContent());
+
+			// BufferedInputStream bis = new BufferedInputStream(response.get);
+			FileOutputStream fos = new FileOutputStream(new File("d:\\kk.fits"));
+
+			byte[] x = new byte[1000];
+			int cont = bis.read(x);
+			while (cont >= 0) {
+				System.out.print(".");
+				fos.write(x, 0, cont);
+				cont = bis.read(x);
+			}
+
+			bis.close();
+			fos.close();
+		}
+
+		
+	}
+
+}
