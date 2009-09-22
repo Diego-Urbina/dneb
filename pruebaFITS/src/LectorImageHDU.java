88f692dafc0e48d59b5753b8d3c3a7f57fdcca7b
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-09-22 20:50:27 +0000

    He hecho algunos métodos que creo que van a resultar útiles para encontrar las estrellas. La media para poder descartar el fondo, y el máximo para saber qué es lo más brillante...

--- pruebaFITS/src/LectorImageHDU.java
+++ pruebaFITS/src/LectorImageHDU.java
@@ -5,8 +5,15 @@ import nom.tam.fits.FitsException;
 public class LectorImageHDU extends LectorHDU {
 
 	// Matriz de pixeles
-	short[][] arrayData;
+	private short[][] arrayData;
+	private short brilloMedio=-1;
+	private short brilloMaximo=-1;
+	private short brilloMinimo=-1;
 	
+
+
+
+
 	public LectorImageHDU(BasicHDU bhdu) {
 		super(bhdu);
 	}
@@ -24,5 +31,87 @@ public class LectorImageHDU extends LectorHDU {
 		}
 		System.out.println("\n");
 	}
+	//Devuelve el brillo medio
+	private short brilloMedio(){
+		
+		long suma=0;
+		
+		for(int i = 0; i < arrayData.length; i++){
+			for(int j = 0; j < arrayData[0].length; j++){
+				suma+=arrayData[i][j];
+			}
+		}
+		return (short) (suma/((arrayData.length)*(arrayData[0].length)));
+	}
+	
+	private short brilloMaximo(){
+		
+		short maximo=-1;
+		
+		for(int i = 0; i < arrayData.length; i++){
+			for(int j = 0; j < arrayData[0].length; j++){
+				if(arrayData[i][j]>maximo){
+					maximo=arrayData[i][j];
+				}
+			}
+		}
+		return maximo;
+	}
+	
+	private short brilloMinimo(){
+		
+		short minimo=-1;
+		
+		for(int i = 0; i < arrayData.length; i++){
+			for(int j = 0; j < arrayData[0].length; j++){
+				if(arrayData[i][j]<minimo){
+					minimo=arrayData[i][j];
+				}
+			}
+		}
+		return minimo;
+	}
+	
+	public short[][] getArrayData() {
+		return arrayData;
+	}
 
+	public void setArrayData(short[][] arrayData) {
+		this.arrayData = arrayData;
+	}
+
+
+	public short getBrilloMedio() {
+		//Si el brillo no se ha solitado todavía lo calculo, si ya se ha calculado devuelvo el valor
+		if(brilloMedio==-1){
+			brilloMedio= brilloMedio();
+			return brilloMedio;
+		}else{
+			return brilloMedio;
+		}
+		
+	}
+
+	public short getBrilloMaximo() {
+		
+		if(brilloMaximo==-1){
+			brilloMaximo= brilloMaximo();
+			return brilloMaximo;
+		}else{
+			return brilloMaximo;
+		}
+	}
+
+	public short getBrilloMinimo() {
+		
+		if(brilloMinimo==-1){
+			brilloMinimo= brilloMinimo();
+			return brilloMinimo;
+		}else{
+			return brilloMinimo;
+		}
+	}
+	
+	
+	
 }
