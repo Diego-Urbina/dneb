d1f12e89dc7643c2127b2db098dd539d5c512888
diego.p.urbina <diego.p.urbina@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-09-24 19:29:19 +0000

    Codigo un poco mas ordenado

--- trunk/pruebaJAI/src/LectorImageHDU.java
+++ trunk/pruebaJAI/src/LectorImageHDU.java
@@ -2,9 +2,6 @@ import nom.tam.fits.BasicHDU;
 import nom.tam.fits.FitsException;
 
 public class LectorImageHDU extends LectorHDU {
-
-	// Matriz de pixeles
-	short[][] arrayData;
 	
 	public LectorImageHDU(BasicHDU bhdu) {
 		super(bhdu);
@@ -13,7 +10,7 @@ public class LectorImageHDU extends LectorHDU {
 	@Override
 	public void leerMatriz() throws FitsException {
 		// Obtengo la matriz de pixeles (Data)
-		arrayData = (short[][]) HDU.getData().getData();
+		short[][] arrayData = (short[][]) HDU.getData().getData();
 		
 		// Recorro la matriz
 		for(int i = 0; i < arrayData.length; i++){
@@ -22,6 +19,36 @@ public class LectorImageHDU extends LectorHDU {
 			System.out.println();
 		}
 		System.out.println("\n");
+	}
+	
+	
+	public void arreglarMatriz(short[][] m, float dataMax, float dataMin) {
+		/* Simetrica de la matriz 'm' respecto del
+		 * eje x. Es necesario hacer esto ya que
+		 * en una imagen FITS, las coordenadas (0,0)
+		 * pertenecen a la esquina inferior izquierda
+		 * y por tanto la primera fila es la de abajo.
+		 */
+		int numFilas = m.length;
+		short temp;
+		for (int i=0; i<numFilas/2; i++)
+			for (int j=0; j<m[i].length; j++){
+				temp = m[i][j];
+				m[i][j] = m[numFilas - 1 - i][j];
+				m[numFilas - 1 - i][j] = temp;
+			}
+		
+		
+		/* Cambio la escala para que el valor del pixel se mueva
+		 * entre 0 y 65535
+		 */
+		float dif = dataMax - dataMin;
+		float scale = (dif) / (2*32767);
+		
+		for (int i=0; i<m.length; i++)
+			for (int j=0; j<m[i].length; j++){
+				m[i][j] = (short) ((m[i][j] - dataMin) / scale);
+			}
 	}
 
 }
--- trunk/pruebaJAI/src/Principal.java
+++ trunk/pruebaJAI/src/Principal.java
@@ -8,9 +8,7 @@ import javax.media.jai.PlanarImage;
 import javax.media.jai.RasterFactory;
 import javax.media.jai.TiledImage;
 
-import nom.tam.fits.Fits;
-import nom.tam.fits.FitsException;
-import nom.tam.fits.ImageHDU;
+import nom.tam.fits.*;
 import nom.tam.util.ArrayFuncs;
 
 public class Principal {
@@ -19,23 +17,22 @@ public class Principal {
 		
 		try {
 			// Cargo la imagen
-			File file = new File("kk.fits");
-			Fits imagenFITS = new Fits(file);			
+			Fits imagenFITS = new Fits(new File("kk.fits"));			
 			
 			// Obtengo el primer HDU (la imagen)
-			ImageHDU imageHDU = (ImageHDU) imagenFITS.getHDU(0);
+			BasicHDU imageHDU = imagenFITS.getHDU(0);
 			
 			// Obtengo la matriz de datos
-			LectorHDU l1 = new LectorImageHDU(imageHDU);
+			LectorImageHDU l1 = new LectorImageHDU(imageHDU);
 			short[][] arrayData = (short[][]) l1.getMatriz();
 			int width = arrayData[0].length;
 			int height = arrayData.length;
 			
-			
-			float dataMin = Float.parseFloat(l1.getValue("DATAMIN"));
-			float dataMax = Float.parseFloat(l1.getValue("DATAMAX"));
-			
-			arreglarMatriz(arrayData, dataMax, dataMin);
+			// Arreglo la matriz
+			l1.arreglarMatriz(
+					arrayData, 									// Matriz de pixeles
+					Float.parseFloat(l1.getValue("DATAMIN")),	// DataMin
+					Float.parseFloat(l1.getValue("DATAMAX")));	// DataMax
 			
 			// La aplano
 			short[] arrayDataAplanado = (short[]) ArrayFuncs.flatten(arrayData);
@@ -49,6 +46,7 @@ public class Principal {
 			TiledImage tiledImage = new TiledImage(0,0,width,height,0,0,sm,cm);
 			tiledImage.setData(raster);
 
+			// Almacena 'tiledImage' en formato PNG
 			JAI.create("filestore",tiledImage,"kk.png","PNG");
 				
 		} catch (FitsException e1) {
@@ -57,34 +55,5 @@ public class Principal {
 			e.printStackTrace();
 		}
 		
-	}
-	
-	public static void arreglarMatriz(short[][] m, float dataMax, float dataMin) {
-		/* Simetrica de la matriz 'm' respecto del
-		 * eje x. Es necesario hacer esto ya que
-		 * en una imagen FITS, las coordenadas (0,0)
-		 * pertenecen a la esquina inferior izquierda
-		 * y por tanto la primera fila es la de abajo.
-		 */
-		int numFilas = m.length;
-		short temp;
-		for (int i=0; i<numFilas/2; i++)
-			for (int j=0; j<m[i].length; j++){
-				temp = m[i][j];
-				m[i][j] = m[numFilas - 1 - i][j];
-				m[numFilas - 1 - i][j] = temp;
-			}
-		
-		
-		/* Cambio la escala para que el valor del pixel se mueva
-		 * entre 0 y 65535
-		 */
-		float dif = dataMax - dataMin;
-		float scale = (dif) / (2*32767);
-		
-		for (int i=0; i<m.length; i++)
-			for (int j=0; j<m[i].length; j++){
-				m[i][j] = (short) ((m[i][j] - dataMin) / scale);
-			}
 	}
 }
