cd4a2f7bbb307693c8b3c7898460e16522af8b00
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-10-03 13:52:55 +0000

    He configurado el script para que se ejecute desde eclipse.

---
+++ HttpImageDownloader/HttpImageDownloaderScriptBBDD.sql
@@ -0,0 +1,9 @@
+delete from survey;
+insert into survey values(1,'poss1_red');
+insert into survey values(2,'poss1_blue');
+insert into survey values(3,'poss2ukstu_blue');
+insert into survey values(4,'poss2ukstu_ir');
+insert into survey values(5,'poss2ukstu_red');
+insert into survey values(6,'quickv&r');
+insert into survey values(7,'phase2_gsc2');
+insert into survey values(8,'phase2_gsc1');
