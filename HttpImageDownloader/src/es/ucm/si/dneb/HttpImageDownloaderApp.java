44582d3c22107a223d96c83e618db84817e5abac
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-10-24 22:24:54 +0000

    Configurado un nuevo sistema de log en ficheros para guardar los errores. Se divide en 10 ficheros, cuando se llena el ultimo sobreescribe el primero.

--- HttpImageDownloader/src/es/ucm/si/dneb/HttpImageDownloaderApp.java
+++ HttpImageDownloader/src/es/ucm/si/dneb/HttpImageDownloaderApp.java
@@ -2,14 +2,19 @@ package es.ucm.si.dneb;
 
 import javax.swing.UnsupportedLookAndFeelException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import es.ucm.si.dneb.gui.VentanaPcpal;
 import es.ucm.si.dneb.service.inicializador.ServicioInicializador;
+import es.ucm.si.dneb.test.ServicioGestionTareasTest;
 
 public class HttpImageDownloaderApp {
-
+	
+	private static final Log LOG = LogFactory
+	.getLog(HttpImageDownloaderApp.class);
 	/**
 	 * @param args
 	 * @throws UnsupportedLookAndFeelException 
@@ -19,6 +24,7 @@ public class HttpImageDownloaderApp {
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		// TODO Auto-generated method stub
+		LOG.debug("INICIO DE LA APLICACION");
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		ServicioInicializador servicioInicializador=(ServicioInicializador) ctx.getBean("servicioInicializador");
 		servicioInicializador.winLookAndFeel();
