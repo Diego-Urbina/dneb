db061e3e45a086de0aee4eccbdf5bc5ef6b3b40e
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-10-31 21:24:47 +0000

    -Anadido fichero .properties con valores por defecto. -Metido el metodo para generar descargas desde datos metidos en base de datos. -Generada clase Util con metodos para diferentes servicios.
    (SIN PROBAR)

--- HttpImageDownloader/src/es/ucm/si/dneb/service/inicializador/ServicioInicializadorImpl.java
+++ HttpImageDownloader/src/es/ucm/si/dneb/service/inicializador/ServicioInicializadorImpl.java
@@ -1,8 +1,10 @@
 package es.ucm.si.dneb.service.inicializador;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,19 +14,27 @@ import javax.swing.plaf.metal.DefaultMetalTheme;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.swing.plaf.metal.OceanTheme;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.ucm.si.dneb.domain.Descarga;
+import es.ucm.si.dneb.domain.FormatoFichero;
 import es.ucm.si.dneb.domain.PuntosRelevantes;
+import es.ucm.si.dneb.domain.Survey;
 import es.ucm.si.dneb.domain.Tarea;
+import es.ucm.si.dneb.service.creacionTareas.ServicioCreacionTareas;
 
 @Service("servicioInicializador")
 public class ServicioInicializadorImpl implements ServicioInicializador {
 	
 	@PersistenceContext
 	EntityManager manager;
+
+	private static final  Log LOG = LogFactory.getLog(ServicioInicializadorImpl.class);
+	
 	
 	final static String THEME = "Test";
 
@@ -160,11 +170,99 @@ public class ServicioInicializadorImpl implements ServicioInicializador {
 	}
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void generarTareaSobreDatosManuales() {
+		
+		LOG.info("GENERANDO DATOS MANUALES DE DESCARGA");
+		
 		List<PuntosRelevantes> puntosRelevantes= manager.createNamedQuery("PuntosRelevantes:dameTodosPuntosRelevantesNoProcesados").getResultList();
 		
+		ResourceBundle resource= ResourceBundle.getBundle("es.ucm.si.dneb.resources");
+		String survey1=resource.getString("survey1");
+		String survey2=resource.getString("survey2");
+		
+		ArrayList<Survey> surveys= new ArrayList<Survey>();
+		Survey surveyold = (Survey) manager.createNamedQuery("Survey:dameSurveyPorDescripcion").setParameter(1, survey1).getSingleResult();
+		Survey surveynew = (Survey) manager.createNamedQuery("Survey:dameSurveyPorDescripcion").setParameter(1, survey2).getSingleResult();
+		
+		surveys.add(surveyold);
+		surveys.add(surveynew);
+		
+		String ruta=resource.getString("directoriodescargas");
+		String ancho=resource.getString("anchopordefecto");
+		String alto=resource.getString("altopordefecto");
+		String solapamiento=resource.getString("solpamientopordefecto");
+		String formatoFichero= resource.getString("formatodefecto");
+		FormatoFichero formato = (FormatoFichero) manager.createNamedQuery("FormatoFichero:dameFormatoPorDescripcion").setParameter(1, formatoFichero).getSingleResult();
+		if(formato==null){
+			LOG.error("FORMATO NO EXISTENTE");
+			
+		}
+		
+		Tarea tarea = new Tarea();
+		tarea.setActiva(false);
+		tarea.setAlto(Double.parseDouble(alto));
+		tarea.setAncho(Double.parseDouble(ancho));
+		tarea.setArFinal("0");
+		tarea.setArInicial("0");
+		tarea.setDecInicial("0");
+		tarea.setDecFinal("0");
+		tarea.setFechaCreacion(this.dameFechaActual());
+		tarea.setFechaUltimaActualizacion(this.dameFechaActual());
+		tarea.setFinalizada(false);
+		tarea.setFormatoFichero(formato);
+		tarea.setRuta(ruta);
+		tarea.setSolpamiento(Double.parseDouble(solapamiento));
+		tarea.setSurveys(surveys);
 		/**TODO**/
+		ArrayList<Descarga> descargas = new ArrayList<Descarga>();
+		
+		for(PuntosRelevantes punto : puntosRelevantes){
+			
+			for(Survey survey : surveys){
+			
+				Descarga descarga = new Descarga();
+				descarga.setAncho(Double.parseDouble(ancho));
+				String ar =new Double(punto.getAscencionRecta()).toString();
+				descarga.setAscensionRecta(ar);
+				String dec = new Double(punto.getDeclinacion()).toString();
+				descarga.setDeclinacion(dec);
+				descarga.setFinalizada(false);
+				descarga.setRutaFichero(creaRuta(tarea.getRuta(), survey.getDescripcion(),
+						ar.toString(), dec.toString(),tarea.getFormatoFichero().getDescipcion()));
+				descarga.setSurvey(survey);
+				descargas.add(descarga);
+			}
+			
+		}
+		tarea.setDescargas(descargas);
+		
+		manager.persist(tarea);
+		
+		LOG.info("FINALIZADA LA GENERACIÓN DE DESCARGAS MANUALES");
 		
 		
 	}
+	private String creaRuta(String rutaBase, String survey,
+			String ascensionRecta, String declinacion, String formato) {
+
+		String ruta = rutaBase;
+		String nombreFichero = null;
+
+		if (ruta != null) {
+			if (rutaBase.charAt(rutaBase.length() - 1) != '/') {
+				rutaBase = rutaBase + "/";
+			}
+			nombreFichero = "AR" + ascensionRecta + "DEC" + declinacion
+					+ "SURV" + survey + "." + formato;
+			ruta = rutaBase + nombreFichero;
+			return ruta;
+
+		} else {
+			return null;
+		}
+
+	}
+
+	
+	
 
 }
