8853e2d7c5adf50a74319f0586a55cd7b12c3d7b
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-11-01 12:59:11 +0000

    Añadidas nuevas trazas de log.

--- trunk/HttpImageDownloader/src/es/ucm/si/dneb/service/creacionTareas/ServicioCreacionTareasImpl.java
+++ trunk/HttpImageDownloader/src/es/ucm/si/dneb/service/creacionTareas/ServicioCreacionTareasImpl.java
@@ -28,72 +28,89 @@ import es.ucm.si.dneb.util.Util;
 
 @Service("servicioCreacionTareas")
 public class ServicioCreacionTareasImpl implements ServicioCreacionTareas {
-	
-	private static final  Log LOG = LogFactory.getLog(ServicioCreacionTareas.class);
-	
+
+	private static final Log LOG = LogFactory
+			.getLog(ServicioCreacionTareas.class);
+
 	@PersistenceContext
 	EntityManager manager;
-	
+
 	@Resource
 	private GestorDescargas gestorDescargas;
-	
+
 	@Transactional(propagation = Propagation.REQUIRED)
-	public void crearTarea(String arInicial,String arFinal,String decInicial,String decFinal,double alto,double ancho,double solapamiento,String surveyOld, String surveynNew,String formato,String ruta){
-		
-		
+	public void crearTarea(String arInicial, String arFinal, String decInicial,
+			String decFinal, double alto, double ancho, double solapamiento,
+			String surveyOld, String surveynNew, String formato, String ruta) {
+
+		LOG.info("COMIENZA LA CREACIÓN DE UNA TAREA: ARINI:" + arInicial
+				+ "ARFIN" + arFinal + "DECINI" + decInicial + "DECFIN"
+				+ decFinal);
 		chequeaSiValoresNoNulos(arInicial, arFinal, decInicial, decFinal, alto,
 				ancho, solapamiento, surveyOld, surveynNew, formato);
-		
-		
-		
+
 		Date fechaActual;
-		
+
 		Tarea tarea = new Tarea();
-		
+
 		fechaActual = Util.dameFechaActual();
-		
-		
+
 		FormatoFichero formatoFichero;
-		
-		try{
-			formatoFichero = (FormatoFichero) manager.createNamedQuery("FormatoFichero:dameFormatoPorDescripcion").setParameter(1, formato).getSingleResult();
-		}catch(NoResultException e){
-			LOG.error("ProblemaQuery,FormatoFichero:dameFormatoPorDescripcion,No se Devuelve resultado");
-			throw new ServicioCreacionTareasException("Prolema al ejecutar query");
-		}catch(NonUniqueResultException e){
-			LOG.error("ProblemaQuery,FormatoFichero:dameFormatoPorDescripcion,Se devuelve más de un resultado");
-			throw new ServicioCreacionTareasException("Prolema al ejecutar query");
+
+		try {
+			formatoFichero = (FormatoFichero) manager.createNamedQuery(
+					"FormatoFichero:dameFormatoPorDescripcion").setParameter(1,
+					formato).getSingleResult();
+		} catch (NoResultException e) {
+			LOG
+					.error("ProblemaQuery,FormatoFichero:dameFormatoPorDescripcion,No se Devuelve resultado");
+			throw new ServicioCreacionTareasException(
+					"Prolema al ejecutar query");
+		} catch (NonUniqueResultException e) {
+			LOG
+					.error("ProblemaQuery,FormatoFichero:dameFormatoPorDescripcion,Se devuelve más de un resultado");
+			throw new ServicioCreacionTareasException(
+					"Prolema al ejecutar query");
 		}
-	    
+
 		ArrayList<Survey> surveys = new ArrayList<Survey>();
-		
-		
-		try{
-			Survey surveyViejo= (Survey) manager.createNamedQuery("Survey:dameSurveyPorDescripcion").setParameter(1, surveyOld).getSingleResult();
+
+		try {
+			Survey surveyViejo = (Survey) manager.createNamedQuery(
+					"Survey:dameSurveyPorDescripcion").setParameter(1,
+					surveyOld).getSingleResult();
 			surveys.add(surveyViejo);
-		}catch(NoResultException e){
-			LOG.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,No se Devuelve resultado");
-			throw new ServicioCreacionTareasException("Prolema al ejecutar query");
-		}catch(NonUniqueResultException e){
-			LOG.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,Se devuelve más de un resultado");
-			throw new ServicioCreacionTareasException("Prolema al ejecutar query");
+		} catch (NoResultException e) {
+			LOG
+					.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,No se Devuelve resultado");
+			throw new ServicioCreacionTareasException(
+					"Prolema al ejecutar query");
+		} catch (NonUniqueResultException e) {
+			LOG
+					.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,Se devuelve más de un resultado");
+			throw new ServicioCreacionTareasException(
+					"Prolema al ejecutar query");
 		}
-		
-		
-		
-		try{
-			Survey surveyActual= (Survey) manager.createNamedQuery("Survey:dameSurveyPorDescripcion").setParameter(1, surveynNew).getSingleResult();
+
+		try {
+			Survey surveyActual = (Survey) manager.createNamedQuery(
+					"Survey:dameSurveyPorDescripcion").setParameter(1,
+					surveynNew).getSingleResult();
 			surveys.add(surveyActual);
-		}catch(NoResultException e){
-			LOG.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,No se Devuelve resultado");
-			throw new ServicioCreacionTareasException("Prolema al ejecutar query");
-		}catch(NonUniqueResultException e){
-			LOG.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,Se devuelve más de un resultado");
-			throw new ServicioCreacionTareasException("Prolema al ejecutar query");
+		} catch (NoResultException e) {
+			LOG
+					.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,No se Devuelve resultado");
+			throw new ServicioCreacionTareasException(
+					"Prolema al ejecutar query");
+		} catch (NonUniqueResultException e) {
+			LOG
+					.error("ProblemaQuery,Survey:dameSurveyPorDescripcion,Se devuelve más de un resultado");
+			throw new ServicioCreacionTareasException(
+					"Prolema al ejecutar query");
 		}
-		
+
 		tarea.setSurveys(surveys);
-		
+
 		tarea.setAlto(alto);
 		tarea.setAncho(ancho);
 		tarea.setArFinal(arFinal);
@@ -105,126 +122,145 @@ public class ServicioCreacionTareasImpl implements ServicioCreacionTareas {
 		tarea.setRuta(ruta);
 		tarea.setActiva(false);
 		tarea.setFinalizada(false);
-		
+
 		tarea.setFechaCreacion(fechaActual);
 		tarea.setFechaUltimaActualizacion(fechaActual);
-		
-		manager.persist(tarea);
-		
-		List<Descarga> descargas = crearDescargas(tarea);
-		
-		tarea.setDescargas(descargas);
-		
-		manager.merge(tarea);
-		
-		gestorDescargas.anadirHilo(tarea);		
-		
-	}
+		try {
+			manager.persist(tarea);
+		} catch (RuntimeException e) {
 
-	
+			LOG.error("NO SE HA PODIDO PERSISTIR LA TAREA");
+
+		}
+		List<Descarga> descargas = crearDescargas(tarea);
+
+		tarea.setDescargas(descargas);
+		try {
+			manager.merge(tarea);
+		} catch (RuntimeException e) {
+
+			LOG.error("NO SE HA PODIDO ACTUALIZAR LA TAREA");
+
+		}
+		gestorDescargas.anadirHilo(tarea);
+
+	}
 
 	@Transactional(propagation = Propagation.SUPPORTS)
 	private void chequeaSiValoresNoNulos(String arInicial, String arFinal,
 			String decInicial, String decFinal, double alto, double ancho,
 			double solapamiento, String surveyOld, String surveynNew,
 			String formato) {
-		if(arInicial==null || arFinal==null || decInicial== null || decFinal==null || alto<=0 ||  ancho<=0 || solapamiento<0 || surveyOld==null || surveynNew==null || formato==null){
+		if (arInicial == null || arFinal == null || decInicial == null
+				|| decFinal == null || alto <= 0 || ancho <= 0
+				|| solapamiento < 0 || surveyOld == null || surveynNew == null
+				|| formato == null) {
 			LOG.error("PARAMETROS NO VÁLIDOS");
-			throw new ServicioCreacionTareasException("Parámetros ilegarles al crear tarea");
+			throw new ServicioCreacionTareasException(
+					"Parámetros ilegarles al crear tarea");
 		}
-		LOG.debug("CHEQUEO DE VALORES NULOS SUPERADO");
+		LOG.info("CHEQUEO DE VALORES NULOS SUPERADO");
 	}
-	
-	
+
 	@Transactional(propagation = Propagation.REQUIRED)
-	public List<Descarga> crearDescargas(Tarea tarea){
-		
+	public List<Descarga> crearDescargas(Tarea tarea) {
+
 		ArrayList<Descarga> descargas = new ArrayList<Descarga>();
-		
-		String ariniS=tarea.getArInicial();
-		Double arini=  Double.parseDouble(ariniS);
-		
-		String deciniS=tarea.getDecInicial();
-		Double decini=  Double.parseDouble(deciniS);
-		
-		String arfinS=tarea.getArFinal();
-		Double arfin=  Double.parseDouble(arfinS);
-		
-		String decfinS=tarea.getDecFinal();
-		Double decfin=  Double.parseDouble(decfinS);
-		
-		Double alto=tarea.getAlto();
-		Double ancho=tarea.getAncho();
-		
-		Double ar=arini;
-		Double dec=decini;
-		
-		Double solap = tarea.getSolpamiento()/100;
-		Double anchoreal=new Double(1D);
-		
+
+		String ariniS = tarea.getArInicial();
+		Double arini = Double.parseDouble(ariniS);
+
+		String deciniS = tarea.getDecInicial();
+		Double decini = Double.parseDouble(deciniS);
+
+		String arfinS = tarea.getArFinal();
+		Double arfin = Double.parseDouble(arfinS);
+
+		String decfinS = tarea.getDecFinal();
+		Double decfin = Double.parseDouble(decfinS);
+
+		Double alto = tarea.getAlto();
+		Double ancho = tarea.getAncho();
+
+		Double ar = arini;
+		Double dec = decini;
+
+		Double solap = tarea.getSolpamiento() / 100;
+		Double anchoreal = new Double(1D);
+
 		LOG.debug("DATOS CREADOS CORRECTAMENTE");
-		
-		while(ar<=arfin){
-			
-			while(dec<=decfin){
-					
-				List<Survey> surveys= tarea.getSurveys();
-				
+
+		while (ar <= arfin) {
+
+			while (dec <= decfin) {
+
+				List<Survey> surveys = tarea.getSurveys();
+
 				LOG.debug("OBTENIDAS TODAS LAS LISTAS DE SURVEYS");
-				
-				for(Survey survey : surveys){
-				
-					Descarga descarga= new Descarga();
+
+				for (Survey survey : surveys) {
+
+					Descarga descarga = new Descarga();
 					descarga.setAscensionRecta(ar.toString());
 					descarga.setDeclinacion(dec.toString());
 					descarga.setFinalizada(false);
-					/**TODO OJO QUE ESTO ES UN CAMBIO IMPORTANTE HAY QUE PROBAR SI FUNCIONA**/
-					descarga.setRutaFichero(Util.creaRuta(tarea.getRuta(), survey.getDescripcion(),
-							ar.toString(), dec.toString(),tarea.getFormatoFichero().getDescipcion()));
+					/**
+					 * TODO OJO QUE ESTO ES UN CAMBIO IMPORTANTE HAY QUE PROBAR
+					 * SI FUNCIONA
+					 **/
+					descarga.setRutaFichero(Util.creaRuta(tarea.getRuta(),
+							survey.getDescripcion(), ar.toString(), dec
+									.toString(), tarea.getFormatoFichero()
+									.getDescipcion()));
 					descarga.setSurvey(survey);
 					descarga.setTarea(tarea);
-					descarga.setAncho((ancho*(Math.cos((dec-(alto/2))))));
-					
+					descarga.setAncho((ancho * (Math.cos((dec - (alto / 2))))));
+
 					manager.persist(descarga);
-					
+
 					descargas.add(descarga);
-					
-					LOG.debug("AÑADIDA LA DESCARGA DEL SURVEY:"+descarga.getSurvey().getDescripcion());
-					
+
+					LOG.debug("AÑADIDA LA DESCARGA DEL SURVEY:"
+							+ descarga.getSurvey().getDescripcion());
+
 				}
-				
+
 				dec = calculaDec(decini, decfin, alto, dec, solap);
-				
+
 			}
-			dec=decini;
-			anchoreal=ancho*(Math.cos((dec * 2.0 * Math.PI)/360.0));
+			dec = decini;
+			anchoreal = ancho * (Math.cos((dec * 2.0 * Math.PI) / 360.0));
 			ar = calculaAr(ar, solap, anchoreal);
-			
+
 		}
 		return descargas;
 	}
 
-
-
 	@Transactional(propagation = Propagation.SUPPORTS)
 	private Double calculaAr(Double ar, Double solap, Double anchoreal) {
-		ar=ar+(anchoreal)-(anchoreal*solap);
+	
+		ar = ar + (anchoreal) - (anchoreal * solap);
+		
+		LOG.debug("CACULA AR :"+ ar.toString());
 		return ar;
 	}
 
 	@Transactional(propagation = Propagation.SUPPORTS)
 	private Double calculaDec(Double decini, Double decfin, Double alto,
 			Double dec, Double solap) {
-		if(decini>decfin){
-			dec=dec-alto+(alto*solap);
-		}else{
-			dec=dec+alto-(alto*solap);
+		if (decini > decfin) {
+			dec = dec - alto + (alto * solap);
+		} else {
+			dec = dec + alto - (alto * solap);
 		}
+		LOG.debug("CACULA DEC :"+ dec.toString());
 		return dec;
 	}
+
 	public void setGestorHilos(GestorDescargas gestorDescargas) {
 		this.gestorDescargas = gestorDescargas;
 	}
+
 	public GestorDescargas getGestorHilos() {
 		return gestorDescargas;
 	}
--- trunk/HttpImageDownloader/src/es/ucm/si/dneb/service/gestionHilos/GestorDescargas.java
+++ trunk/HttpImageDownloader/src/es/ucm/si/dneb/service/gestionHilos/GestorDescargas.java
@@ -8,25 +8,35 @@ import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.*;
 
 import es.ucm.si.dneb.domain.Tarea;
+import es.ucm.si.dneb.service.creacionTareas.ServicioCreacionTareas;
 import es.ucm.si.dneb.service.gestionTareas.ServicioGestionTareas;
 
 @Service("gestorHilos")
 public class GestorDescargas{
 	
+	private static final Log LOG = LogFactory
+	.getLog(GestorDescargas.class);
+
 	private HashMap <Long,Hilo> hilos;
 	ExecutorService service = Executors.newFixedThreadPool(6);
 	
 	public GestorDescargas(){
+		LOG.debug("GESTOR DESCARGAS CREADO CON CERO HILOS");
 		hilos=new HashMap<Long,Hilo>();
 	}
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void anadirHilo(Tarea tarea) {
+		
+		LOG.debug("AÑADIR HILO:"+tarea.getIdTarea());
+		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
 		"applicationContext.xml");
 		EjecutorTarea gestor=(EjecutorTarea)ctx.getBean("ejecutorTarea");
@@ -36,11 +46,14 @@ public class GestorDescargas{
 		Hilo hilo = new Hilo(gestor);
 		hilos.put(tarea.getIdTarea(), hilo);
 		
+		LOG.info("HILO CREADO:"+ tarea.getIdTarea());
+		
 	}
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void crearHilosParaTodasLasTareas(List<Tarea> tareas) {
 		
+		LOG.debug("LLAMADA A CREAR HILOS PARA TODAS LAS DESCARGAS");
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(
 		"applicationContext.xml");
 		for(Tarea tarea : tareas){
@@ -52,10 +65,14 @@ public class GestorDescargas{
 			hilos.put(tarea.getIdTarea(), hilo);
 		}
 		
+		LOG.info("TAREAS CREADAS PARA TODAS LAS DESCARGAS");
+		
 	}
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void eleminarHilo(Long id) {
+		
+		
 		Hilo hilo=hilos.get(id);
 		
 		if(hilo.isInterrupted()==false){
@@ -63,7 +80,7 @@ public class GestorDescargas{
 		}
 		hilos.remove(id);
 		
-		
+		LOG.info("ELIMINADO EL HILO:" +id);
 	}
 
 	
@@ -76,6 +93,8 @@ public class GestorDescargas{
 		Hilo hilo=hilos.get(idHilo);
 		service.execute(hilo);
 		
+		LOG.info("INICIADO EL HILO:" +idHilo);
+		
 	}
 
 	@Transactional(propagation = Propagation.REQUIRED)
@@ -85,8 +104,7 @@ public class GestorDescargas{
 		if(hilo.isInterrupted()==false){
 			hilo.interrupt();
 		}
-		//hilos.remove(idHilo);
-		
+		LOG.info("INTERRUMPIDO EL HILO:" + idHilo);
 	}
 
 
