81a9f6513fb8b4626032cb74ba74dff2f82301ab
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-11-16 19:14:54 +0000

    Intento de empezar a integrarlo todo en la misma aplicacion, de momento tampoco hay que hacerle mucho caso.

---
+++ trunk/HttpImageDownloader/src/es/ucm/si/dneb/service/image/centroid/CalculateCentroid.java
@@ -0,0 +1,10 @@
+package es.ucm.si.dneb.service.image.centroid;
+
+import es.ucm.si.dneb.service.image.util.Point;
+import es.ucm.si.dneb.service.image.util.Star;
+
+public interface CalculateCentroid <P,I>{
+	
+	public P giveMeTheCentroid(I points);
+
+}
