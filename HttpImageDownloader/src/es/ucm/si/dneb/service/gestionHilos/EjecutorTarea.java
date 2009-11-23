d7e6309bb3fa10d5612c25e71f0af0f3030ba488
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-11-23 12:41:39 +0000

    En el camino de funcionar pero por supuesto falla.jajajja

--- HttpImageDownloader/src/es/ucm/si/dneb/service/gestionHilos/EjecutorTarea.java
+++ HttpImageDownloader/src/es/ucm/si/dneb/service/gestionHilos/EjecutorTarea.java
@@ -9,6 +9,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -29,6 +30,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import es.ucm.si.dneb.domain.Descarga;
 import es.ucm.si.dneb.domain.Tarea;
+import es.ucm.si.dneb.service.downloadImage.ServiceDownloadImage;
 
 @Service("ejecutorTarea")
 @Scope("prototype")
@@ -43,123 +45,9 @@ public class EjecutorTarea {
 	@PersistenceContext
 	EntityManager manager;
 	
+	@Resource
+	private ServiceDownloadImage serviceDownloadImage;
 	
-	@Transactional(propagation = Propagation.SUPPORTS)
-	public void downloadImage(String survey, String ascensionRecta,
-			String declinacion, String equinocio, String alto, String ancho,
-			String formato, String compresion, String ruta) {
-
-		LOG.info("Entrada en el método downloadImage");
-		final HttpClient httpclient = new DefaultHttpClient();
-
-		final List<BasicNameValuePair> formparams = new ArrayList<BasicNameValuePair>(
-				10);
-		formparams.add(new BasicNameValuePair("v", survey));
-		formparams.add(new BasicNameValuePair("r", ascensionRecta));
-		formparams.add(new BasicNameValuePair("d", declinacion));
-		formparams.add(new BasicNameValuePair("e", equinocio));
-		formparams.add(new BasicNameValuePair("h", alto));
-		formparams.add(new BasicNameValuePair("w", ancho));
-		formparams.add(new BasicNameValuePair("f", formato));
-		formparams.add(new BasicNameValuePair("c", compresion));
-		formparams.add(new BasicNameValuePair("fov", "none"));
-		formparams.add(new BasicNameValuePair("v3", ""));
-
-		UrlEncodedFormEntity entity = null;
-		try {
-			entity = new UrlEncodedFormEntity(formparams, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			LOG.debug("Problema con la codificación de los parámetros");
-			e.printStackTrace();
-		}
-
-		LOG.info("Parámetros configurados correctamente");
-		final HttpPost httppost = new HttpPost(
-				"http://archive.stsci.edu/cgi-bin/dss_search");
-
-		httppost.setEntity(entity);
-
-		HttpResponse response = null;
-		try {
-			response = httpclient.execute(httppost);
-		} catch (ClientProtocolException e) {
-			LOG.debug("ERROR EJECUTANDO HTTPPOST");
-			e.printStackTrace();
-		} catch (IOException e) {
-			LOG.debug("ERROR EJECUTANDO HTTPPOST");
-			e.printStackTrace();
-		}
-
-		LOG.info("HTTPPOST EJECUTADO SATISFACTORIAMENTE");
-
-		final HttpEntity resEnt = response.getEntity();
-		if (resEnt != null) {
-			BufferedInputStream bis = null;
-			try {
-				bis = new BufferedInputStream(resEnt.getContent());
-			} catch (IllegalStateException e) {
-
-				e.printStackTrace();
-			} catch (IOException e) {
-
-				e.printStackTrace();
-			}
-
-			FileOutputStream fos = null;
-			try {
-				/** TODO PONER LA RUTA DE MANERA CORRECTA **/
-				fos = new FileOutputStream(new File(ruta));
-			} catch (FileNotFoundException e) {
-				LOG.debug("ERROR AL CREAR EL FICHERO");
-				e.printStackTrace();
-			}
-			LOG.info("Se procede a la lecuta de datos");
-			final byte[] buffer = new byte[1024];
-			int cont = 0;
-			try {
-				cont = bis.read(buffer);
-			} catch (IOException e) {
-				LOG.debug("ERROR AL LEER DATOS DE LA URL");
-				e.printStackTrace();
-			}
-			int total = cont;
-			while (cont >= 0) {
-				LOG.info("Leidos " + cont);
-				try {
-					fos.write(buffer, 0, cont);
-				} catch (IOException e) {
-					LOG.debug("ERROR AL ESCRIBIR DATOS EN EL FICHERO");
-					e.printStackTrace();
-				}
-				try {
-					cont = bis.read(buffer);
-				} catch (IOException e) {
-					LOG.debug("ERROR AL LEER DATOS DE LA URL");
-					e.printStackTrace();
-				}
-				total += cont;
-			}
-			LOG.info("Fin de la lectura de datos");
-			LOG.info("Leidos: " + total + " bytes");
-			try {
-				bis.close();
-			} catch (IOException e) {
-				LOG.debug("ERROR AL CERRAR EL FICHERO");
-				e.printStackTrace();
-			}
-			try {
-				fos.close();
-			} catch (IOException e) {
-				LOG.debug("ERROR AL CERRAR EL FICHERO");
-				e.printStackTrace();
-			}
-			LOG.info("Fichero cerrado");
-		} else {
-			LOG.debug("PROBLEMA OBTENIENDO RESPONSE ENTITY");
-
-		}
-
-	}
 	
 
 	public void setIdTarea(Long idTarea) {
@@ -177,15 +65,7 @@ public class EjecutorTarea {
 	public Tarea getTarea() {
 		return tarea;
 	}
-	//@Transactional(propagation = Propagation.REQUIRED)
-	public void ejecutar(){
-		List<Descarga> descargas = getDownloads();
-		for (Descarga descarga : descargas) {
-			startDownload(descarga);
-		}
-		endTask();
-	}
-
+	
 	//@Transactional(propagation= Propagation.SUPPORTS)
 	private List<Descarga> getDownloads() {
 		List<Descarga> descargas = manager.createNamedQuery(
@@ -193,22 +73,32 @@ public class EjecutorTarea {
 		tarea.getIdTarea()).getResultList();
 		return descargas;
 	}
+	
+	
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void ejecutar(){
+		List<Descarga> descargas = getDownloads();
+		for (Descarga descarga : descargas) {
+			/*startDownload(descarga);*/
+			serviceDownloadImage.startDownload(descarga.getIdDescarga(), tarea.getAlto(), tarea.getFormatoFichero().getDescipcion());
+		}
+		endTask();
+	}
+
+
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	private void endTask() {
 		tarea.setFinalizada(true);
 	}
 
-	@Transactional(propagation = Propagation.REQUIRED)
-	private void startDownload(Descarga descarga) {
-		downloadImage(descarga.getSurvey().getDescripcion(), descarga
-		.getAscensionRecta().toString(), descarga.getDeclinacion()
-		.toString(), "J2000", Double.toString(tarea.getAlto()),
-		Double.toString(descarga.getAncho()), tarea
-				.getFormatoFichero().getDescipcion(), "none",
-		descarga.getRutaFichero());
-		
-		descarga.setFinalizada(true);
-		manager.merge(descarga);
+	public void setServiceDownloadImage(ServiceDownloadImage serviceDownloadImage) {
+		this.serviceDownloadImage = serviceDownloadImage;
 	}
+
+	public ServiceDownloadImage getServiceDownloadImage() {
+		return serviceDownloadImage;
+	}
+
+	
 }
