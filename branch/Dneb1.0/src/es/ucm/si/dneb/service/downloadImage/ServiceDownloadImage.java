8065c9e406dc087ca63ceb0d9a02e2361131cbdc
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2009-12-30 21:09:51 +0000

    Me equivoque

--- branch/Dneb1.0/src/es/ucm/si/dneb/service/downloadImage/ServiceDownloadImage.java
+++
@@ -1,8 +0,0 @@
-package es.ucm.si.dneb.service.downloadImage;
-
-public interface ServiceDownloadImage {
-	
-	
-	public void startDownload(Long idDescarga,double alto,String formatoFichero);
-
-}
--- branch/Dneb1.0/src/es/ucm/si/dneb/service/math/MathService.java
+++
@@ -1,9 +0,0 @@
-package es.ucm.si.dneb.service.math;
-
-public interface MathService{
-	
-	public Distance calculateDecimalDistance(Double ar1, Double dec1,Double ar2,Double dec2);
-	
-	public double[][] transform(double image[][], double scale,double rotation,double verticalTranslation,double horizontalTranslation);
-
-}
