04741333e248861c9c630acc9eb2154f913b53f8
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-01-17 23:10:44 +0000

    COPIA SEGURIDAD IMPORTAR CATALOGO POR TERMINAR

--- Dneb/src/es/ucm/si/dneb/service/importData/ImportDoubleStarCatalogImpl.java
+++ Dneb/src/es/ucm/si/dneb/service/importData/ImportDoubleStarCatalogImpl.java
@@ -1,6 +1,11 @@
 package es.ucm.si.dneb.service.importData;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.ucm.si.dneb.domain.DoubleStarCatalog;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -9,18 +14,25 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Service("importDoubleStarCatalog")
 public class ImportDoubleStarCatalogImpl implements ImportDoubleStarCatalog{
 
-
+	@PersistenceContext
+	EntityManager manager;
+	
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void deleteCatalog() {
 		// TODO Auto-generated method stub
 		
 	}
 
-
+	@Transactional(propagation=Propagation.REQUIRED)
 	public ArrayList<String> loadCatalogToString(String fileName) {
 		
 		  	 if ((fileName == null) || (fileName == ""))
@@ -50,15 +62,36 @@ public class ImportDoubleStarCatalogImpl implements ImportDoubleStarCatalog{
 		        return file;
 	}
 	
-	
-	public void createCatalog(String path){
+	@Transactional(propagation=Propagation.REQUIRED)
+	public void loadCatalog(String path){
 		
 		ArrayList<String> dsc = this.loadCatalogToString(path);
 		
 		
+		
+		for(String doubleStar : dsc ){	
+			
+			
+				//TODO
+				DoubleStarCatalog doubleStarCatalog= new DoubleStarCatalog();
+				
+				doubleStarCatalog.setCoordinates(doubleStar.substring(0, 10));
+				doubleStarCatalog.setDiscovererAndNumber(doubleStar.substring(10, 17));
+				doubleStarCatalog.setComponents(doubleStar.substring(17,22));
+				doubleStarCatalog.setFirstObservation(Date.valueOf(doubleStar.substring(24,27).trim()+"-01-01"));
+				//doubleStarCatalog.setLastObservation(doubleStar.substring(28,32))
+				doubleStarCatalog.setNumObservations(Integer.decode(doubleStar.substring(33,37).trim()));
+				//doubleStarCatalog.setFirstPosAngle(Double.parseDouble(doubleStar.substring(38,42).trim()));
+				
+				manager.persist(doubleStarCatalog);
+			
+			
+		}
+		
+		
 	}
 
-
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void updateCatalog(String filePath) {
 		// TODO Auto-generated method stub
 		
