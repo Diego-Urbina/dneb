607dd03609e854d5f4f3aad5005ab2858ada5060
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-01-18 09:17:29 +0000

    Servicio para importar un catálogo terminado, falta revisar si los fallos que tiene son culpa del fichero o de un mal parseo.

--- trunk/Dneb/src/es/ucm/si/dneb/service/importData/ImportDoubleStarCatalogImpl.java
+++ trunk/Dneb/src/es/ucm/si/dneb/service/importData/ImportDoubleStarCatalogImpl.java
@@ -1,17 +1,15 @@
 package es.ucm.si.dneb.service.importData;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.ucm.si.dneb.domain.DoubleStarCatalog;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Date;
@@ -25,6 +23,9 @@ public class ImportDoubleStarCatalogImpl implements ImportDoubleStarCatalog{
 
 	@PersistenceContext
 	EntityManager manager;
+	
+	private static final  Log LOG = LogFactory.getLog(ImportDoubleStarCatalogImpl.class);
+	
 	
 	@Transactional(propagation=Propagation.REQUIRED)
 	public void deleteCatalog() {
@@ -78,10 +79,88 @@ public class ImportDoubleStarCatalogImpl implements ImportDoubleStarCatalog{
 				doubleStarCatalog.setCoordinates(doubleStar.substring(0, 10));
 				doubleStarCatalog.setDiscovererAndNumber(doubleStar.substring(10, 17));
 				doubleStarCatalog.setComponents(doubleStar.substring(17,22));
-				doubleStarCatalog.setFirstObservation(Date.valueOf(doubleStar.substring(24,27).trim()+"-01-01"));
-				//doubleStarCatalog.setLastObservation(doubleStar.substring(28,32))
+				try{
+					doubleStarCatalog.setFirstObservation(Date.valueOf(doubleStar.substring(23,27).trim()+"-01-01"));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(23-27) : " + doubleStar);
+				}
+				try{
+					doubleStarCatalog.setLastObservation(Date.valueOf(doubleStar.substring(28,32).trim()+"-01-01"));
+					
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(28-32) :" + doubleStar);
+				}
+				
 				doubleStarCatalog.setNumObservations(Integer.decode(doubleStar.substring(33,37).trim()));
-				//doubleStarCatalog.setFirstPosAngle(Double.parseDouble(doubleStar.substring(38,42).trim()));
+				try{
+					doubleStarCatalog.setFirstPosAngle(Double.parseDouble(doubleStar.substring(38,41).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(38-41) :" + doubleStar);
+				}
+				
+				try{
+					doubleStarCatalog.setLastPosAnges(Double.parseDouble(doubleStar.substring(42,45).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(42-45) :" + doubleStar);
+				}
+				
+				try{
+					doubleStarCatalog.setFirstSeparation(Double.parseDouble(doubleStar.substring(46,51).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(46-51) :" + doubleStar);
+				}
+				
+				try{
+					doubleStarCatalog.setLastSeparation(Double.parseDouble(doubleStar.substring(52,57).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(52-57) :" + doubleStar);
+				}
+				
+				try{
+					doubleStarCatalog.setFirstStarMagnitude(Double.parseDouble(doubleStar.substring(58,63).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(58-63) :" + doubleStar);
+				}
+				
+				try{
+					doubleStarCatalog.setSecondStarMagnitude(Double.parseDouble(doubleStar.substring(64,69).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(64-69) :" + doubleStar);
+				}
+				
+				doubleStarCatalog.setSpectralType(doubleStar.substring(70,79));
+				
+				try{
+					doubleStarCatalog.setPrimaryProperMotionRa(Double.parseDouble(doubleStar.substring(80,84).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(80-84) :" + doubleStar);
+				}
+				
+				try{
+					doubleStarCatalog.setPrimaryProperMotionDec(Double.parseDouble(doubleStar.substring(84,88).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(84-88) :" + doubleStar);
+				}
+				
+				
+				try{
+					doubleStarCatalog.setSecondaryProperMotionRa(Double.parseDouble(doubleStar.substring(89,93).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(89-93) :" + doubleStar);
+				}
+				
+				try{
+					doubleStarCatalog.setSecondaryProperMotionDec(Double.parseDouble(doubleStar.substring(93,97).trim()));
+				}catch(Exception e){
+					LOG.debug("PROBLEM IMPORTING DATA OF DOUBLE STAR CATALOG:(93-97) :" + doubleStar);
+				}
+				
+				
+				doubleStarCatalog.setDurchmusterungNumber(doubleStar.substring(98,106));
+				
+				doubleStarCatalog.setNotes(doubleStar.substring(107,111));
+				
+				doubleStarCatalog.setArcsecondCoordinates2000(doubleStar.substring(112,130));
 				
 				manager.persist(doubleStarCatalog);
 			
@@ -95,6 +174,10 @@ public class ImportDoubleStarCatalogImpl implements ImportDoubleStarCatalog{
 	public void updateCatalog(String filePath) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public static Log getLog() {
+		return LOG;
 	}
 	
 
