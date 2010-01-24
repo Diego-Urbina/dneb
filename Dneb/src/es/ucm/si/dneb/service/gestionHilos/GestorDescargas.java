89afe029346350555de56a204706b99b2c175b40
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-01-24 12:58:37 +0000

    Cambio en los gestores de hilos, lo que había no estaba bien.

--- Dneb/src/es/ucm/si/dneb/service/gestionHilos/GestorDescargas.java
+++ Dneb/src/es/ucm/si/dneb/service/gestionHilos/GestorDescargas.java
@@ -2,66 +2,118 @@ package es.ucm.si.dneb.service.gestionHilos;
 
 import java.util.HashMap;
 
-
 import java.util.List;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.*;
 
 import es.ucm.si.dneb.domain.Tarea;
 import es.ucm.si.dneb.service.inicializador.ContextoAplicacion;
 
 @Service("gestorDescargas")
-public class GestorDescargas extends GestorHilos{
-	
-	private static final Log LOG = LogFactory
-	.getLog(GestorDescargas.class);
-	
-	public GestorDescargas() {
-		super();
-		super.setNombreGestor("GESTOR DESCARGAS");
-	}
+public class GestorDescargas implements GestorHilos<Tarea> {
 
+	private static final Log LOG = LogFactory.getLog(GestorDescargas.class);
+
+	private HashMap<Long, Hilo> hilos;
+
+	public GestorDescargas() {
+		hilos = new HashMap<Long, Hilo>();
+	}
 
 	public void anadirHilo(Tarea tarea) {
-		
-		LOG.debug("AÑADIR HILO:"+tarea.getIdTarea());
-		
+
+		LOG.debug("AÑADIR HILO:" + tarea.getIdTarea());
+
 		ApplicationContext ctx = ContextoAplicacion.getApplicationContext();
-		EjecutorTarea gestor=(EjecutorTarea)ctx.getBean("ejecutorDescarga");
+		EjecutorTarea gestor = (EjecutorTarea) ctx.getBean("ejecutorDescarga");
 		gestor.setTarea(tarea);
 		gestor.setIdTarea(tarea.getIdTarea());
-		
+
 		Hilo hilo = new Hilo(gestor);
 		hilos.put(tarea.getIdTarea(), hilo);
-		
-		LOG.info("HILO CREADO:"+ tarea.getIdTarea());
-		
+
+		LOG.info("HILO CREADO:" + tarea.getIdTarea());
+
 	}
 
-	
 	public void crearHilosParaTodasLasTareas(List<Tarea> tareas) {
-		
+
 		LOG.debug("LLAMADA A CREAR HILOS PARA TODAS LAS DESCARGAS");
 		ApplicationContext ctx = ContextoAplicacion.getApplicationContext();
-		for(Tarea tarea : tareas){
-			EjecutorTarea gestor=(EjecutorTarea)ctx.getBean("ejecutorDescarga");
+		for (Tarea tarea : tareas) {
+			EjecutorTarea gestor = (EjecutorTarea) ctx
+					.getBean("ejecutorDescarga");
 			gestor.setTarea(tarea);
 			gestor.setIdTarea(tarea.getIdTarea());
-			
+
 			Hilo hilo = new Hilo(gestor);
 			hilos.put(tarea.getIdTarea(), hilo);
 		}
-		
+
 		LOG.info("TAREAS CREADAS PARA TODAS LAS DESCARGAS");
-		
+
 	}
 
+	@Override
+	public void eleminarHilo(Long id) {
+		Hilo hilo = hilos.get(id);
+
+		if (hilo.isInterrupted() == false) {
+
+			LOG.info("GESTOR" + hilo.continuar());
+
+			hilo.parar();
+
+			LOG.info("GESTOR" + hilo.continuar());
+
+			hilo.interrupt();
+		}
+
+		hilos.remove(id);
+
+		LOG.info("GESTOR" + "ELIMINADO EL HILO:" + id);
+
+	}
+
+	@Override
+	public void iniciarHilo(Long idHilo) {
+		Hilo hilo = hilos.get(idHilo);
+
+		hilo.start();
+
+		LOG.info("GESTOR INICIADO EL HILO:" + idHilo);
+
+	}
+
+	@Override
+	public void interrumpirHilo(Long idHilo) {
+		Hilo hilo = hilos.get(idHilo);
+
+		Tarea tarea = hilo.getEjecutor().getTarea();
+
+		this.eleminarHilo(idHilo);
+
+		this.anadirHilo(tarea);
+
+		LOG.info("GESTOR   nombreGestor INTERRUMPIDO EL HILO:" + idHilo);
+
+	}
+
+	public HashMap<Long, Hilo> getHilos() {
+		return hilos;
+	}
+
+	public void setHilos(HashMap<Long, Hilo> hilos) {
+		this.hilos = hilos;
+	}
+
+	public static Log getLog() {
+		return LOG;
+	}
 	
+	
+
 }
--- Dneb/src/es/ucm/si/dneb/service/gestionHilos/GestorHilos.java
+++ Dneb/src/es/ucm/si/dneb/service/gestionHilos/GestorHilos.java
@@ -10,91 +10,21 @@ import org.springframework.context.ApplicationContext;
 import es.ucm.si.dneb.domain.Tarea;
 import es.ucm.si.dneb.service.inicializador.ContextoAplicacion;
 
-public abstract class GestorHilos {
-
-	HashMap <Long,Hilo> hilos;
-	
-	private String nombreGestor;
-	
-	private static final Log LOG = LogFactory
-	.getLog(GestorHilos.class);
+public interface GestorHilos<T>{
 
 	
-	public GestorHilos(){
-		LOG.debug("GESTOR" + nombreGestor +"CREADO CON 0 HILOS");
-		hilos=new HashMap<Long,Hilo>();
-	}
-	
-	public abstract void anadirHilo(Tarea tarea);
+	public abstract void anadirHilo(T t);
 
 	
-	public abstract void crearHilosParaTodasLasTareas(List<Tarea> tareas);
+	public void eleminarHilo(Long id) ;
 
 	
-	public void eleminarHilo(Long id) {
-		
-		
-		Hilo hilo=hilos.get(id);
-		
-		if(hilo.isInterrupted()==false){
-			
-			
-			LOG.info("GESTOR" + nombreGestor +hilo.continuar());
-			
-			hilo.parar();
-			
-			LOG.info("GESTOR" + nombreGestor +hilo.continuar());
-			
-			hilo.interrupt();
-		}
-		
-		hilos.remove(id);
-		
-		LOG.info("GESTOR" + nombreGestor +"ELIMINADO EL HILO:" +id);
-	}
-
-	
-	public HashMap<Long, Hilo> getHilos() {
-		return hilos;
-	}
-
-	
-	public void iniciarHilo(Long idHilo) {
-		Hilo hilo=hilos.get(idHilo);
-		
-		hilo.start();
-		
-		LOG.info("GESTOR" + nombreGestor +"INICIADO EL HILO:" +idHilo);
-		
-	}
-
-	public void interrumpirHilo(Long idHilo) {
-		
-		Hilo hilo=hilos.get(idHilo);
-		
-		Tarea tarea= hilo.getEjecutor().getTarea();
-		
-		this.eleminarHilo(idHilo);
-		
-		this.anadirHilo(tarea);
-		
-		LOG.info("GESTOR" + nombreGestor +"INTERRUMPIDO EL HILO:" + idHilo);
-	}
-
-
-	public void setHilos(HashMap<Long, Hilo> hilos) {
-		this.hilos = hilos;
-		
-	}
-
-	public String getNombreGestor() {
-		return nombreGestor;
-	}
-
-	public void setNombreGestor(String nombreGestor) {
-		this.nombreGestor = nombreGestor;
-	}
+	public void iniciarHilo(Long idHilo);
 	
 	
+	public void interrumpirHilo(Long idHilo);
+
+
+
 	
 }
