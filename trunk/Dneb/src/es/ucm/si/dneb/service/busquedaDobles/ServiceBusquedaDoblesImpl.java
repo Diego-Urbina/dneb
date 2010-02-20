e4850b7e66b9bd8b9876517ab9c6dcfde0ccbd56
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-02-20 18:47:09 +0000

    Intento de sincronizar los últimos cambios pero en las próixmas habrá problemas por colisiones.

---
+++ trunk/Dneb/src/es/ucm/si/dneb/service/busquedaDobles/ServiceBusquedaDoblesImpl.java
@@ -0,0 +1,14 @@
+package es.ucm.si.dneb.service.busquedaDobles;
+
+import org.springframework.stereotype.Service;
+
+@Service("serviceBusquedaDobles")
+public class ServiceBusquedaDoblesImpl implements ServiceBusquedaDobles{
+
+	@Override
+	public void iniciarProcesamiento(Long idProcImg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- trunk/Dneb/src/es/ucm/si/dneb/service/gestionHilos/EjecutorProcesamiento.java
+++ trunk/Dneb/src/es/ucm/si/dneb/service/gestionHilos/EjecutorProcesamiento.java
@@ -1,26 +1,75 @@
 package es.ucm.si.dneb.service.gestionHilos;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import es.ucm.si.dneb.domain.Imagen;
 import es.ucm.si.dneb.domain.ProcesamientoImagen;
 import es.ucm.si.dneb.domain.Tarea;
 import es.ucm.si.dneb.domain.TareaProcesamiento;
+import es.ucm.si.dneb.service.busquedaDobles.ServiceBusquedaDobles;
+import es.ucm.si.dneb.service.calculoPosicion.ServiceCalculoPosicion;
 
 @Service("ejecutorCalculoPosicion")
 @Scope("prototype")
 @Transactional(propagation=Propagation.SUPPORTS)
 public class EjecutorProcesamiento implements EjecutorTarea<TareaProcesamiento>{
 	
+	@Resource
+	private ServiceBusquedaDobles serviceBusquedaDobles;
+	
+	@Resource
+	private ServiceCalculoPosicion serviceCalculoPosicion;
 	
 	private TareaProcesamiento tareaProcesamiento;
 	private Long tareaProcesamientoId ;
 	
-	@Override
+	private static final Log LOG = LogFactory
+    .getLog(EjecutorProcesamiento.class);
+	
+	@PersistenceContext
+    EntityManager manager;
+	
+	private List<ProcesamientoImagen> getDownloads() {
+		//TODO
+		return null;
+	}
+	
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void ejecutar(Interrumpible inter) {
-		// TODO Auto-generated method stub
+		
+		 List<ProcesamientoImagen> imagens = getDownloads();
+         for (ProcesamientoImagen imagen : imagens) {
+                 
+        	 
+                 if(inter.continuar()){
+                	 if(this.tareaProcesamiento.getTipoProcesamiento().getIdTipoProcesamiento()==1){
+                		 
+                		//TODO serviceBusquedaDobles
+                		 
+                	 }else{
+                		 
+                		//TODO serviceCalculoPosicion
+                	 }
+                }else{
+                         break;
+                 }
+                 
+         }
+         if(inter.continuar()){
+        	 	tareaProcesamiento.setFinalizada(true);
+                manager.merge(tareaProcesamiento);
+         }
 		
 	}
 	@Override
@@ -38,6 +87,26 @@ public class EjecutorProcesamiento implements EjecutorTarea<TareaProcesamiento>{
 	@Override
 	public void setId(Long id) {
 		this.tareaProcesamientoId=id;
+	}
+
+	public void setServiceBusquedaDobles(ServiceBusquedaDobles serviceBusquedaDobles) {
+		this.serviceBusquedaDobles = serviceBusquedaDobles;
+	}
+
+	public ServiceBusquedaDobles getServiceBusquedaDobles() {
+		return serviceBusquedaDobles;
+	}
+
+	public void setServiceCalculoPosicion(ServiceCalculoPosicion serviceCalculoPosicion) {
+		this.serviceCalculoPosicion = serviceCalculoPosicion;
+	}
+
+	public ServiceCalculoPosicion getServiceCalculoPosicion() {
+		return serviceCalculoPosicion;
+	}
+
+	public static Log getLog() {
+		return LOG;
 	}
 	
 	
