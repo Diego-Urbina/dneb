54762fedb79cb2b39352cf850521f714472ca1a2
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-02-22 23:31:27 +0000

    Algun cambio chorra para la mejora de la eficiencia pk en cada procesamiento imagen comprobabamos el tipo de procesamiento, eso con hacerlo una vez basta, de todos modos falta probarlo.

--- Dneb/src/es/ucm/si/dneb/service/calculoPosicion/ServiceCalculoPosicion.java
+++ Dneb/src/es/ucm/si/dneb/service/calculoPosicion/ServiceCalculoPosicion.java
@@ -1,6 +1,10 @@
 package es.ucm.si.dneb.service.calculoPosicion;
 
+import es.ucm.si.dneb.domain.ProcesamientoImagen;
+
 public interface ServiceCalculoPosicion {
+	
+	public void calcularPosicion(ProcesamientoImagen pi);
 
 
 }
--- Dneb/src/es/ucm/si/dneb/service/calculoPosicion/ServiceCalculoPosicionImpl.java
+++ Dneb/src/es/ucm/si/dneb/service/calculoPosicion/ServiceCalculoPosicionImpl.java
@@ -1,8 +1,18 @@
 package es.ucm.si.dneb.service.calculoPosicion;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.ucm.si.dneb.domain.ProcesamientoImagen;
 
 @Service("serviceCalculoPosicion")
 public class ServiceCalculoPosicionImpl implements ServiceCalculoPosicion{
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void calcularPosicion(ProcesamientoImagen pi) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
