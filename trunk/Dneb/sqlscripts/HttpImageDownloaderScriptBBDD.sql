4e08a79c943e0b15a6e08862485831250aa04bec
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-03-11 20:45:10 +0000

    Nuevo script de base de datos.

--- trunk/Dneb/sqlscripts/HttpImageDownloaderScriptBBDD.sql
+++ trunk/Dneb/sqlscripts/HttpImageDownloaderScriptBBDD.sql
@@ -4,9 +4,9 @@ insert into Survey values(2,'poss1_blue');
 insert into Survey values(3,'poss2ukstu_blue');
 insert into Survey values(4,'poss2ukstu_ir');
 insert into Survey values(5,'poss2ukstu_red');
-insert into Survey values(6,'quickv&r');
-insert into Survey values(7,'phase2_gsc2');
-insert into Survey values(8,'phase2_gsc1');
+-- insert into Survey values(6,'quickv&r');
+-- insert into Survey values(7,'phase2_gsc2');
+-- insert into Survey values(8,'phase2_gsc1');
 
 delete from FormatoFichero;
 insert into FormatoFichero values(1,'fits','fits');
