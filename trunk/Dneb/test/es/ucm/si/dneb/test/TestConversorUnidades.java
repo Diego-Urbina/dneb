fb6be913cf2b432044b5acee82fd3a095a8d1095
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-03-21 15:31:06 +0000

    Cambios en pruebas de conversión de unidades

--- trunk/Dneb/test/es/ucm/si/dneb/test/TestConversorUnidades.java
+++ trunk/Dneb/test/es/ucm/si/dneb/test/TestConversorUnidades.java
@@ -1,5 +1,7 @@
 package es.ucm.si.dneb.test;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 import es.ucm.si.dneb.service.math.CoordinateConverter;
@@ -8,17 +10,27 @@ import es.ucm.si.dneb.service.math.SexagesimalCoordinate;
 
 public class TestConversorUnidades {
 	
+	private static final Log LOG = LogFactory
+	.getLog(TestConversorUnidades.class);
+
+	
 	@Test
 	public void testConvSexToDec(){
+		
 		SexagesimalCoordinate sc = new SexagesimalCoordinate(21, 28, 19.85, 36, 40, 6.7);
-		System.out.println(sc.toString());
+		LOG.info(sc.toString());
 		DecimalCoordinate dc = CoordinateConverter.sexagesimalToDecimalConverter(sc);
-		System.out.println(dc.toString());
+		LOG.info(dc.toString());
 		
 		sc=CoordinateConverter.decimalToSexagesimalConverter(dc);
 		
-		System.out.println(sc.toString());
+		LOG.info(sc.toString());
 		
+	}
+
+
+	public static Log getLog() {
+		return LOG;
 	}
 
 }
