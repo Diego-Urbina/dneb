6ce535b0d58044732be4121d74c9f743081ec67e
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-03-27 15:32:07 +0000

    Corregidos algunos errores en el conversor cuando se daban valores negativos.

--- Dneb/src/es/ucm/si/dneb/service/math/CoordinateConverter.java
+++ Dneb/src/es/ucm/si/dneb/service/math/CoordinateConverter.java
@@ -10,7 +10,10 @@ public class CoordinateConverter {
 		RA= ((RAHours*3600+RAMins*60+RASecs)/240)*/
 
 		dc.setAr( (double)( (sc.getArh()*3600) + (sc.getArmin()*60) + (sc.getArsec() ) )/240);
-		dc.setDec((sc.getDech()<0 ? -1 : 1 ) * Math.abs(sc.getDech())+( (double)sc.getDecmin()/60)+((double)sc.getDecsec()/3600) );
+		
+		/*SI(D29>0;D29+E29/60+F29/3600; D29-E29/60-F29/3600)*/
+		//dc.setDec(((double)(sc.getDech()<0 ? -1 : 1 ) * (double)Math.abs(sc.getDech()))+( (double)sc.getDecmin()/60)+((double)sc.getDecsec()/3600) );
+		dc.setDec(sc.getDech()>0 ? sc.getDech()+((double)sc.getDecmin()/60)+ ((double)sc.getDecsec()/3600): sc.getDech()-((double)sc.getDecmin()/60)-((double)sc.getDecsec()/3600));
 		
 		return dc;
 	}
@@ -21,11 +24,11 @@ public class CoordinateConverter {
 		
 		sc.setArh((int) ((dc.getAr()*24)/360));
 		sc.setArmin((int)((dc.getAr()-(15*sc.getArh()))*4));
-		sc.setArsec((dc.getAr() - (((sc.getArh()*60) +(sc.getArmin()))/4))*240 );
+		sc.setArsec((dc.getAr() - ((double)((sc.getArh()*60) +(sc.getArmin()))/4))*240 );
 		
 		
-		sc.setDech((dc.getDec()<0 ? (int) (dc.getDec()+1) : (int) (dc.getDec())));
-		sc.setDecmin((int)((((double)dc.getDec()-(double)sc.getDech()))*60));
+		sc.setDech((dc.getDec()<0 ? (int) (dc.getDec()) : (int) (dc.getDec())));
+		sc.setDecmin((int)(Math.abs((((double)dc.getDec()-(double)sc.getDech())))*60));
 		sc.setDecsec( (dc.getDec()*3600)>((sc.getDech()*3600)-(sc.getDecmin()*60))? ((dc.getDec()*3600)-(sc.getDech()*3600)-(sc.getDecmin()*60)):( (-dc.getDec()*3600)+(sc.getDech()*3600)-(sc.getDecmin()*60) ));
 		
 		return sc;
