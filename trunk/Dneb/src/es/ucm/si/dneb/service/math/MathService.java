fa5b264e2f485ba5b3d7ac8ad6916557e681ffae
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-04-26 14:59:17 +0000

    Ivan te dejo el método que hay que llamar para calcular la distancia, sólo tienes que pasarle los dos puntos, es decir ar1 dec1 ar2 dec2 y ya está.

--- trunk/Dneb/src/es/ucm/si/dneb/service/math/MathService.java
+++ trunk/Dneb/src/es/ucm/si/dneb/service/math/MathService.java
@@ -1,9 +1,13 @@
 package es.ucm.si.dneb.service.math;
 
+import es.ucm.si.dneb.domain.Imagen;
+
 public interface MathService{
 	
 	public Distance calculateDecimalDistance(Double ar1, Double dec1,Double ar2,Double dec2);
 	
 	public double[][] transform(double image[][], double scale,double rotation,double verticalTranslation,double horizontalTranslation);
+	
+	public DecimalCoordinate pixelToCoordinatesConverter(Imagen imagen, int width, int height, double x, double y);
 
 }
