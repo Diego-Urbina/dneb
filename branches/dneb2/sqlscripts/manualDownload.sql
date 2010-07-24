75ca62955802e6240d66b3e4f27561477bfdc6e7
dgodmun <dgodmun@f53169da-6275-11de-8ac7-2bb2ed267d8e>
2010-07-24 15:35:21 +0000

    Algunos bugs solucionados.

--- branches/dneb2/sqlscripts/manualDownload.sql
+++ branches/dneb2/sqlscripts/manualDownload.sql
@@ -1,6 +1,6 @@
-insert into puntosrelevantes(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (20,20,"kk1",false);
-insert into puntosrelevantes(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (30,20,"kk2",false);
-insert into puntosrelevantes(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (40,20,"kk3",false);
-insert into puntosrelevantes(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (50,20,"kk4",false);
-insert into puntosrelevantes(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (60,20,"kk5",false);
-insert into puntosrelevantes(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (70,20,"kk6",false);
+insert into carga_datos(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (20,20,"kk1",false);
+insert into carga_datos(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (30,20,"kk2",false);
+insert into carga_datos(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (40,20,"kk3",false);
+insert into carga_datos(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (50,20,"kk4",false);
+insert into carga_datos(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (60,20,"kk5",false);
+insert into carga_datos(ASCENCIONRECTA, DECLINACION, DESCRIPCION, PROCESADO) values (70,20,"kk6",false);
